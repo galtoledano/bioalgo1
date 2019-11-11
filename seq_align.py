597c83a85129a1088497b237089d4f7df4ecd1bf
gal <gal.toledano@mail.huji.ac.il>
2019-11-11 09:36:59 +0200

    local align

--- seq_align.py
+++ seq_align.py
@@ -24,23 +24,26 @@ def score(score_matrix, i, j):
     return score_matrix[i][j]
 
 
-def global_align(col, row, seq2, seq1, score_matrix):
+def align(col, row, seq2, seq1, score_matrix, globaling=True):
     values = np.zeros((row, col), dtype=int)
     pointers = np.zeros((row, col), dtype=int)
-    init(col, pointers, row, score_matrix, seq1, seq2, values)
-    build_matrix(col, pointers, row, score_matrix, seq1, seq2, values)
-    eli1, eli2 = traceback(col, pointers, row, seq1, seq2)
-
+    if globaling:
+        init(col, pointers, row, score_matrix, seq1, seq2, values)
+    build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling)
+    print(values)
+    eli1, eli2 = traceback(col, pointers,values, row, seq1, seq2, globaling)
     printall(eli1, eli2, pointers, values)
 
 
-def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values):
+def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling):
     for i in range(1, row):
         for j in range(1, col):
             d = values[i - 1][j - 1] + score(score_matrix, seq1[i - 1], seq2[j - 1])
             h = values[i - 1][j] + score(score_matrix, GAP, seq2[j - 1])
             v = values[i][j - 1] + score(score_matrix, seq1[i - 1], GAP)
             arr = np.array([v, h, d])
+            if not globaling:
+                arr = np.append(arr, 0)
             pointers[i][j] = np.argmax(arr) + 1
             values[i][j] = np.max(arr)
 
@@ -68,11 +71,17 @@ def printall(eli1, eli2, pointers, values):
         print(eli2[len(eli2) - i - 1], end=" ")
 
 
-def traceback(col, pointers, row, seq1, seq2):
+def traceback(col, pointers,values, row, seq1, seq2, glob):
     eli1 = []
     eli2 = []
     i = row - 1
     j = col - 1
+    if not glob:
+        vals = np.array(values)
+        res = np.where(vals == np.amax(vals))
+        res = np.array(res)
+        i = res[0][0]
+        j = res[1][0]
     p = pointers[i][j]
     while p != 0:
         if p == 1:
@@ -94,17 +103,21 @@ def traceback(col, pointers, row, seq1, seq2):
     return eli1, eli2
 
 
+
 def main():
-    score = read_score("score_matrix.tsv")
-    global_align(4, 5, "AGC", "AAAC", score)
-# parser = argparse.ArgumentParser()
-    # parser.add_argument('seq_a', help='Path to first FASTA file (e.g. fastas/HomoSapiens-SHH.fasta)')
-    # parser.add_argument('seq_b', help='Path to second FASTA file')
+    # parser = argparse.ArgumentParser()
+    # seq_a = parser.add_argument('seq_a', help='Path to first FASTA file (e.g. fastas/HomoSapiens-SHH.fasta)')
+    # seq_b = parser.add_argument('seq_b', help='Path to second FASTA file')
     # parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
     # parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
+    score = read_score("score_matrix.tsv")
+    # align(4, 5, "AGC", "AAAC", score)  # to remember, len +1
+    align(5, 6, "AAGA", "TTAAG", score, False)  # to remember, len +1
+
     # command_args = parser.parse_args()
+    #     global_align(len(seq_a), len(seq_b), seq_a, seq_b, score)
     # if command_args.align_type == 'global':
-    #     raise NotImplementedError
+        # raise NotImplementedError
     # elif command_args.align_type == 'local':
     #     raise NotImplementedError
     # elif command_args.align_type == 'overlap':
