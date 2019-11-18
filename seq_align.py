5c699a89b59b02a281e3eeb9aecdab3292effdbe
gal <gal.toledano@mail.huji.ac.il>
2019-11-18 20:12:14 +0200

    finished general code

--- seq_align.py
+++ seq_align.py
@@ -3,6 +3,12 @@ import numpy as np
 import pandas as pd
 from itertools import groupby
 
+D = 1
+
+H = 2
+
+V = 3
+
 GAP = "-"
 
 
@@ -21,7 +27,7 @@ def read_score(file):
     return score
 
 
-def score(score_matrix, i, j):
+def score(score_matrix, i, j, row_num, ind_i, ind_j, overlap):
     """
     gets two letters, returning it's score
     :param score_matrix:  the matrix with all scores
@@ -29,10 +35,12 @@ def score(score_matrix, i, j):
     :param j: the second letter
     :return: the score of i, j.
     """
+    if overlap and (ind_i == 0):
+        return 0
     return score_matrix[i][j]
 
 
-def align(col, row, seq2, seq1, score_matrix, globaling=True):
+def align(col, row, seq2, seq1, score_matrix, globaling, overlap):
     """
     Calculates the optimal sequence alignment according to the selected alignment type
     :param col: the numbers of letters at the first sequence
@@ -41,20 +49,21 @@ def align(col, row, seq2, seq1, score_matrix, globaling=True):
     :param seq1: the first sequence
     :param score_matrix: the matrix with all scores
     :param globaling: Decides the type of alignment. If true- global, if false- local
+    :param overlap: Decides the type of alignment. If true- overlaping
     :return:
     """
     #  init the matrixes
     values = np.zeros((row, col), dtype=int)
     pointers = np.zeros((row, col), dtype=int)
     if globaling:
-        init(col, pointers, row, score_matrix, seq1, seq2, values)
-    build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling)
+        init(col, pointers, row, score_matrix, seq1, seq2, values, overlap)
+    build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling, overlap)
     # print(values)
-    eli1, eli2 = traceback(col, pointers,values, row, seq1, seq2, globaling)
-    # printall(eli1, eli2, pointers, values)
+    eli1, eli2 = traceback(col, pointers,values, row, seq1, seq2, globaling, overlap)
+    printall(eli1, eli2, pointers, values)
 
 
-def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling):
+def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling, overlap):
     """
     building the matrix according the formula
     :param col: the numbers of letters at the first sequence
@@ -65,13 +74,14 @@ def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling
     :param seq2: the second sequence
     :param values: the value's matrix
     :param globaling: Decides the type of alignment. If true- global, if false- local
+    :param overlap: Decides the type of alignment. If true- overlap.
     :return:
     """
     for i in range(1, row):
         for j in range(1, col):
-            d = values[i - 1][j - 1] + score(score_matrix, seq1[i - 1], seq2[j - 1])  #diagonal
-            h = values[i - 1][j] + score(score_matrix, GAP, seq2[j - 1])  #horizontal
-            v = values[i][j - 1] + score(score_matrix, seq1[i - 1], GAP)  #vertical
+            d = values[i - 1][j - 1] + score(score_matrix, seq1[i - 1], seq2[j - 1], row-1, i, j, overlap)  #diagonal
+            h = values[i][j - 1] + score(score_matrix, GAP, seq2[j - 1], row-1, i, j, overlap)  #horizontal
+            v = values[i - 1][j] + score(score_matrix, seq1[i - 1], GAP, row-1, i, j, overlap)  #vertical
             arr = np.array([v, h, d])
             if not globaling:
                 arr = np.append(arr, 0)  #at local alignment, also can be 0
@@ -79,7 +89,9 @@ def build_matrix(col, pointers, row, score_matrix, seq1, seq2, values, globaling
             values[i][j] = np.max(arr)
 
 
-def init(col, pointers, row, score_matrix, seq1, seq2, values):
+
+
+def init(col, pointers, row, score_matrix, seq1, seq2, values, overlap):
     """
     Initializes two arrays. the first array used to hold formula results values. The second array is to save pointers
     for trace back the sequence.
@@ -93,12 +105,16 @@ def init(col, pointers, row, score_matrix, seq1, seq2, values):
     :return:
     """
     for i in range(row):
-        values[i][0] = score(score_matrix, seq1[i - 1], GAP) * i
         pointers[i][0] = 1
+        # if overlap:
+        #     continue
+        values[i][0] = score(score_matrix, seq1[i - 1], GAP, row-1, i, 0, overlap) * i
     for j in range(col):
-        values[0][j] = score(score_matrix, GAP, seq2[j - 1]) * j
-        pointers[0][j] = 2
+        values[0][j] = score(score_matrix, GAP, seq2[j - 1], row-1, 0, j, overlap) * j
+        pointers[0][j] = H
     pointers[0][0] = 0
+    print(pointers)
+
 
 
 def printall(eli1, eli2, pointers, values):
@@ -110,12 +126,12 @@ def printall(eli1, eli2, pointers, values):
     print("eli1 :")
     for i in range(len(eli1)):
         print(eli1[len(eli1) - i - 1], end=" ")
-    print("eli2 :")
+    print("\neli2 :")
     for i in range(len(eli2)):
         print(eli2[len(eli2) - i - 1], end=" ")
 
 
-def traceback(col, pointers,values, row, seq1, seq2, glob):
+def traceback(col, pointers,values, row, seq1, seq2, glob, overlap):
     """
     trace back according to the pointers matrix and getting the best scored sequence
 
@@ -139,21 +155,33 @@ def traceback(col, pointers,values, row, seq1, seq2, glob):
         res = np.array(res)
         i = res[0][0]
         j = res[1][0]
+    if overlap:
+        print(values)
+        vals = np.array(values)
+        res = np.where(vals == np.amax(vals[i]))
+        res = np.array(res)
+        j = res[1][0]
     p = pointers[i][j]
+    print("pointers:")
+    print(pointers)
+    if overlap and (j < col - 1):
+        align2 = [GAP] * (col - j - 1)
+        temp = list(seq2)
+        align1 = temp[j:col-1][::-1]
     while p != 0:
-        if p == 1:
+        if int(p) == 1:
             align1.append(GAP)
-            align2.append(seq2[j - 1])
-            j -= 1
-            p = pointers[i][j]
-        elif p == 2:
-            align1.append(seq1[i - 1])
-            align2.append(GAP)
+            align2.append(seq1[i - 1])
             i -= 1
             p = pointers[i][j]
-        elif p == 3:
-            align1.append(seq1[i - 1])
-            align2.append(seq2[j - 1])
+        elif int(p) == 2:
+            align1.append(seq2[j - 1])
+            align2.append(GAP)
+            j -= 1
+            p = pointers[i][j]
+        elif int(p) == 3:
+            align1.append(seq2[j - 1])
+            align2.append(seq1[i - 1])
             i -= 1
             j -= 1
             p = pointers[i][j]
@@ -168,8 +196,10 @@ def main():
     # parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
     # parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
     score = read_score("score_matrix.tsv")
-    # align(4, 5, "AGC", "AAAC", score)  # to remember, len +1
-    align(5, 6, "AAGA", "TTAAG", score, False)  # to remember, len +1
+    # align(4, 5, "AGC", "AAAC", score, True, False)  # to remember, len +1
+    # align(8, 4, "AAAGCCG", "CCG", score, True, True)  # to remember, len +1
+    align(5, 6, "AAGA", "TTAAG", score, False, False)  # to remember, len +1
+
 
     # command_args = parser.parse_args()
     #     global_align(len(seq_a), len(seq_b), seq_a, seq_b, score)
